cb8a24624e2bd4c45fc0e75028d888c3471fcc5f
MohamadAlGhorani <moh.gh266@gmail.com>
2021-08-27 11:29:59 +0200

    add intro text

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".logo-button")
 const video = document.querySelector(".intro-video")
+const introText = document.querySelector(".intro-text")
+const mainEl = document.querySelector(".main")
+
 
 btn.addEventListener('click' , function(){
     if(!!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2)){
@@ -16,4 +19,7 @@ btn.addEventListener('click' , function(){
 video.addEventListener("ended" , function(){
     video.classList.remove("active")
     btn.classList.remove("dance")
+    btn.classList.add("hide")
+    introText.classList.add("show")
+    mainEl.classList.add("is-white")
 })
